076e316063356ca94b4a45108c97d10ab5023d71
madD7 <sdhaval.3003@gmail.com>
2019-08-16 14:39:47 +0530

    Log library updated to log time and function name. Signal handler library completed

---
+++ Framework/Inc/singalhandler.h
@@ -0,0 +1,75 @@
+/****************************************
+Description	: 
+
+Revision History **************************************************************************
+** Date ** ** Coder ** ***** Remarks ******************************************************
+* 290717 * ** madD7 ** * Originator                                                       *
+
+*/
+
+
+
+#ifndef		__SIGNALHANDLER_H_
+#define		__SIGNALHANDLER_H_
+
+
+
+/* Include Files **************************************************************************
+* @{
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <sys/resource.h>
+#include "constants.h"
+#include "log.h"
+/*
+* @}
+*/
+
+
+/* Data Types & Variables *****************************************************************
+ * @{
+ */
+
+/***************************************************************************************
+ LOG LEVEL MACROS
+*/
+
+typedef RETVAL (*FuncSignalHandler_I_I) (INT32);
+
+typedef RETVAL (*FuncAppExitHandler_V_I)(void);
+
+
+/*
+ * @}
+ */
+
+
+
+/* Functions ******************************************************************************
+ * @{
+ */
+
+
+/***************************************************************************************
+ LOG FUNCTION LIKE MACROS
+*/
+
+INT32 SIG_SendSig(pid_t iProcId, INT32 iSigNum);
+RETVAL SIG_Init(FuncSignalHandler_I_I pfnSig, FuncAppExitHandler_V_I pfnAppEx);
+
+/*
+* @}
+*/
+
+
+#endif
+
+
+/* ~~~~~ END OF FILE ~~~~~ */
+
