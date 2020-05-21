4631c8a22bdc33ea5b5c7940f63bca44efc57f34
madD7 <sdhaval.3003@gmail.com>
2020-05-21 18:24:50 +0530

    Single script for User management

--- Framework/Scripts/Administration/ManageUser.sh
+++ Framework/Scripts/Administration/ManageUser.sh
@@ -9,7 +9,8 @@
 #					Optionally, you may provide a comment for the account as
 #					an argument to the script.
 #					The script will automatically generate a random password.
-
+#
+#					NOTE: " quotes around the variable name may not seem justified. 
 #Revision History **************************************************************************
 #** Date ** ** Coder ** ***** Remarks ******************************************************
 #* DDMMYY * ** madD7 ** * Originator                                                       *
@@ -26,7 +27,7 @@ SPC_CHARS_STRING=`echo "!@#$%^&*()_+"`
 #Check for the Root permissions
 CheckRootPermission()
 {
-	if [ "${UID}" -ne 0 ]
+	if [[ "${UID}" -ne 0 ]]
 	then 
 		LogErrFunc "2" "You do not sufficient permissions. This will be reported."
 		exit 2
@@ -39,11 +40,12 @@ PrintHelp()
 {
 	echo "Usage: ${0} [OPTIONS]... <username> [Comment]"
 	echo "-a Creates an account on local system with <UserName>"
+	echo "   with comments, if any." 
 	echo "-c Used to add Comments while adding a new user"
 	echo "-d Delete user"
-	echo "-r Revoke/Disable user"
 	echo "-b Backup user directory to archieve path. Used with delete user option only."
-	echo "with comments, if any." 
+	echo "-r Remove home directory"
+	echo "-t Temporarily Disable user"
 	exit 1
 }
 
@@ -54,12 +56,12 @@ PrintHelp()
 GetRandomNumWithIn()
 {
 	# Check if the range is passed to the function
-	if [ -z "${1}" ]
+	if [[ -z "${1}" ]]
 	then
 		TRUNCATE=false
 	fi
 
-	if [ "${1}" -ge 1 ]
+	if [[ "${1}" -ge 1 ]]
 	then 
 		TRUNCATE=true
 		MOD="${1}"
@@ -67,16 +69,16 @@ GetRandomNumWithIn()
 		TRUNCATE=false
 	fi
 
-	while [ true ]
+	while [[ true ]]
 	do
 		RANDOM_DIV=`echo ${RANDOM}`
 
-		if [ "${TRUNCATE}" == "true" ]
+		if [[ "${TRUNCATE}" == "true" ]]
 		then
 			RANDOM_DIV=`echo $(( ${RANDOM_DIV} % ${MOD}))`
 		fi
 
-		if [ "${RANDOM_DIV}" -ge 1 ]
+		if [[ "${RANDOM_DIV}" -ge 1 ]]
 		then
 			return ${RANDOM_DIV}
 		fi
@@ -136,12 +138,12 @@ AddUsr()
 {
 	# add user
 	useradd -c "${COMMENTS}" -m "${USR_NAME}"
-	RETVAL="${?}"
+	RETVAL=${?}
 
 	case ${RETVAL} in
 		9)
 			LogFunc "${RETVAL}" "User [${USR_NAME}] already exists"
-			return "${RETVAL}"
+			return ${RETVAL}
 			;;
 
 		0)
@@ -150,7 +152,7 @@ AddUsr()
 
 		?)
 			LogFunc "${RETVAL}" "Failed to create account for User [${USR_NAME}]"
-			return "${RETVAL}"
+			return ${RETVAL}
 			;;
 	esac
 
@@ -161,11 +163,11 @@ AddUsr()
 	# instead use chpasswd 
 	echo "${USR_NAME}:${TEMP_PASSWD}" | sudo chpasswd	
 
-	RETVAL="${?}"
-	if [ "${RETVAL}" -ne 0 ]
+	RETVAL=${?}
+	if [[ "${RETVAL}" -ne 0 ]]
 	then 
 		LogFunc "${RETVAL}" "Unable to set password for ${USR_NAME}"
-		return "${RETVAL}"
+		return ${RETVAL}
 	fi
 
 	# Expire passwd immediately. This forces user to 
@@ -175,27 +177,33 @@ AddUsr()
 	return 0
 }
 
+
 ############# MAIN ###############
 
-CheckRootPermission
-
 # Check arguments passed 
-if [ "${#}" -lt 1 ]
+if [[ "${#}" -lt 1 ]]
 then
 	PrintHelp
+	LogFunc "2" "No arguments passed"
+	exit 2
 fi
 
+ADD_USR=0
+DELETE_USR=0
+DISABLE_USR=0
+
 # get all arrguments 
-while getopts a:bc:d:r: OPTIONS
+while getopts a:b:c:d:rt: OPTIONS
 do
 	case ${OPTIONS} in
 		a)
-			ADD_USR='true'
+			ADD_USR=1
 			USR_NAME="${OPTARG}"
 			;;
 
 		b)
 			BACKUP='true'
+			ARCHIVE_PATH="${OPTARG}"
 			;;
 
 		c)
@@ -204,12 +212,17 @@ do
 			;;
 
 		d)
-			DELETE_USR='true'
+			DELETE_USR=1
 			USR_NAME="${OPTARG}"
 			;;
 
 		r)
-			REVOKE_USR='true'
+			REMOVE_HOME_DIR="-r"
+			;;
+
+		## Temporarily disable user 
+		t)
+			DISABLE_USR=1
 			USR_NAME="${OPTARG}"
 			;;
 
@@ -221,22 +234,26 @@ do
 	esac
 done
 
+CheckRootPermission
+
 # Check Username string length
-if [ -z "${USR_NAME}" ]
+if [[ -z "${USR_NAME}" ]]
 then
 	LogErrFunc "1" "Invalid username [${USR_NAME}] string"
 	exit 1
 fi
 
-if [ "${ADD_USR}" == "true" && "${DELETE_USR}" == "true" ]
+TEMP=$(( ${ADD_USR} + ${DELETE_USR} + ${DISABLE_USR} ))
+if [[ "${TEMP}" -ne 1 ]]
 then
-	LogFunc "2" "Invalid Arguments"
+	echo "-a -t and -d can not be used simultaneously"
+	LogFunc "2" "Use either Add/Delete/Disable"
 	PrintHelp
 	exit 2
 fi
 
 # Check operation to be performed
-if [ "${ADD_USR}" == "true" ]
+if [[ "${ADD_USR}" -eq 1 ]]
 then
 
 	# The remaining arguments will be treated as comments
@@ -248,31 +265,103 @@ then
 	GenPasswd
 
 	AddUsr
-	RETVAL="${?}"
-	if [ "${RETVAL}" -ne 0 ] 
+	RETVAL=${?}
+	if [[ "${RETVAL}" -ne 0 ]] 
 	then
 		exit "${RETVAL}" 
 	fi
 
 	LogFunc "${RETVAL}" "Created new user [${USR_NAME}] with Passwd [${TEMP_PASSWD}] on Host [${HOSTNAME}]"
 
-elif [ "${DELETE_USR}" == "true" ]
+elif [[ "${DELETE_USR}" -eq 1 ]]
 then
 	USR_ID=`id -u "${USR_NAME}"`
-	
+	RETVAL=${?}
+	if [[ "${RETVAL}" -ne 0 ]]
+	then
+		LogFunc "${RETVAL}" "Invalid Username [${USR_NAME}]"
+		exit ${RETVAL}
+	fi
+
 	# UserID must be 1000 or greater.
 	# UserID less than 1000 are reserved for System account
 	if [ "${USR_ID}" -lt 1000 ]
 	then
 		LogErrFunc "2" "Permission deined to delete User [${USR_NAME}] [${USR_ID}]"
-		return 2
+		exit 2
 	fi
 
-	if 
+	if [[ "${BACKUP}" == "true" ]]
+	then
+		# Check if path exists
+		if [[ ! -d "${ARCHIVE_PATH}" ]]
+		then
+			mkdir -p "${ARCHIVE_PATH}"
+			RETVAL=${?}
+			if [[ "${RETVAL}" -ne 0 ]]
+			then
+				LogErrFunc "${RETVAL}" "Unable to create Archive Path [${ARCHIVE_PATH}]"
+				exit ${RETVAL}
+			fi
+		fi
 
+		#Check for user home directory
+		USR_HOME_PATH="/home/${USR_NAME}"
+		if [[ -d "${USR_HOME_PATH}" ]]
+		then
+			ARCHIVE_FILE="${ARCHIVE_PATH}/${USR_NAME}.tgz"
+			tar -zcf ${ARCHIVE_FILE} ${USR_HOME_PATH} > /dev/null  
+			RETVAL=${?}
+			if [[ ${RETVAL} -ne 0 ]]
+			then
+				LogErrFunc "${RETVAL}" "Failed to create Archive"
+				exit ${RETVAL}
+			fi
 
-elif [ "${REVOKE_USR}" == "true" ]
+		else
+			LogTeeFunc "1" "User home directory [${USR_HOME_PATH}] not found"
+		fi
+	fi
+
+	# Delete user
+	userdel ${REMOVE_HOME_DIR} ${USR_NAME}
+	RETVAL=${?}
+	if [[ ${RETVAL} -ne 0 ]]
+	then
+		LogErrFunc "${RETVAL}" "Unable to delete user [${USR_NAME}]"
+		exit ${RETVAL}
+	fi
+
+	LogFunc "${RETVAL}" "User [${USR_NAME}] deleted successfully"
+
+elif [[ "${DISABLE_USR}" -eq 1 ]]
 then
+	USR_ID=`id -u "${USR_NAME}"`
+	RETVAL=${?}
+	if [[ "${RETVAL}" -ne 0 ]]
+	then
+		LogFunc "${RETVAL}" "Invalid Username [${USR_NAME}]"
+		exit ${RETVAL}
+	fi
+
+    # UserID must be 1000 or greater.
+    # UserID less than 1000 are reserved for System account
+    if [ "${USR_ID}" -lt 1000 ]
+    then
+        LogErrFunc "2" "Permission deined to delete User [${USR_NAME}] [${USR_ID}]"
+        exit 2
+    fi
+	
+	# Change User expiry date 
+	chage -E 0 ${USR_NAME}
+	RETVAL=${?}
+	if [[ ${RETVAL} -ne 0 ]]
+	then
+		LogErrFunc "${RETVAL}" "Unable to disable user [${USR_NAME}]"
+		exit ${RETVAL}
+	fi
+
+	LogFunc "${RETVAL}" "User [${USR_NAME}] account disabled successfully"	
 fi
 
 
